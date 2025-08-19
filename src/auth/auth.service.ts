cf53c66404121f3626bf290addcbd72c952e4633
Henok-Aragaw <henokaragaw17@gmail.com>
2025-08-19 19:00:17 +0300

    feat: register

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,61 @@
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+
+@Injectable()
+export class AuthService {
+    constructor(private prisma: PrismaService){}
+
+    //Register user
+    async register(registerDto: RegisterDto){
+        const {email, password} = registerDto
+
+        //check if the user exist
+        const existingUser = await this.prisma.user.findUnique({
+            where: {email}
+        });
+        
+    if(existingUser){
+        throw new ConflictException('User already exists')
+    }
+
+    //hash password 
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    //create new user
+    const newUser = await this.prisma.user.create({
+        data: {
+            email,
+            password: hashedPassword
+        },
+    });
+
+    //remove password from return object
+    const {password: _, ...result} = newUser;
+
+    return result;
+    }
+
+    async login(loginDto:LoginDto){
+        const {email, password } = loginDto;
+        
+        //Find the current user by email as email is an unique property
+        const user = await this.prisma.user.findUnique({
+            where:{email}
+        });
+
+        if(!user){
+            throw new UnauthorizedException("Invalid credentials! Please try again")
+        }
+
+        //verify password
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(!isPasswordValid){
+            throw new UnauthorizedException("Invalid credentials! Please try again")
+        }
+
+
+    }
+}
---
+++ src/auth/dto/login.dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail, IsString,IsNotEmpty, MinLength } from "class-validator";
+
+
+export class LoginDto {
+    @IsEmail({}, {message:'Please provide a valid email address'})
+    email:string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(6, {message: 'Password must be at least 6 characters long'})
+    password:string;
+}
